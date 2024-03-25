db08638e9319c08f327bf05a7543763c7df97c33
jamiesellman <james.sellman12@gmail.com>
2024-03-25 17:41:30 +0000

    Added further functionality, to be able to search via a search bar and also filter by a drop down bar

--- components/CreateProduct.tsx
+++ components/CreateProduct.tsx
@@ -1,11 +1,11 @@
-import { SyntheticEvent, useState } from "react"
-import axios from 'axios'
-import { useNavigate } from "react-router-dom"
-import React from "react"
+import { SyntheticEvent, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import React from "react";
 
 export default function CreateProduct() {
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   // ! Our state should look like a movie now.
   const [formData, setFormData] = useState({
     title: "",
@@ -14,100 +14,107 @@ export default function CreateProduct() {
     description: "",
     unitsSold: "",
     category: "",
-  })
+  });
 
   function handleChange(e: any) {
-    const fieldName = e.target.name
-    const newFormData = structuredClone(formData)
-    newFormData[fieldName as keyof typeof formData] = e.target.value
-    newFormData.unitsSold = "0"
-    setFormData(newFormData)
+    const fieldName = e.target.name;
+    const newFormData = structuredClone(formData);
+    newFormData[fieldName as keyof typeof formData] = e.target.value;
+    newFormData.unitsSold = "0";
+    setFormData(newFormData);
   }
 
   async function handleSubmit(e: SyntheticEvent) {
-    e.preventDefault()
+    e.preventDefault();
     // ! We now need to provide a TOKEN in the request. We get this from localStorage
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem("token");
     // ? If you do need to send the year back as a number, this is how you would do it.
     // const newFormData = structuredClone(formData)
     // newFormData.year = Number(newFormData.year)
 
     // ! Here we attach the token to the request to the API.
-    const resp = await axios.post('/api/products', formData, {
-      headers: { Authorization: `Bearer ${token}` }
-    })
-    console.log(resp.data)
+    const resp = await axios.post("/api/products", formData, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    console.log(resp.data);
     // ! We're now going to movies
-    navigate('/products')
+    navigate("/products");
   }
 
-  console.log(formData)
+  console.log(formData);
 
-  return <div className="section">
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <div className="field">
-          <label className="label">Product Name</label>
-          <div className="control">
-            <input
-              className="input"
-              type="text"
-              name={'title'} // ! Updating name field to the right name
-              onChange={handleChange}
-              value={formData.title} // ! Updating the value to the right bit of formData
-            />
+  return (
+    <div className="section">
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <div className="field">
+            <label className="label">Product Name</label>
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                name={"title"} // ! Updating name field to the right name
+                onChange={handleChange}
+                value={formData.title} // ! Updating the value to the right bit of formData
+              />
+            </div>
           </div>
-        </div>
-        <div className="field">
-          <label className="label">Price</label>
-          <div className="control">
-            <input
-              className="input"
-              type="text"
-              name={'price'} // ! Updating name field to the right name
-              onChange={handleChange}
-              value={formData.price} // ! Updating the value to the right bit of formData
-            />
+          <div className="field">
+            <label className="label">Price</label>
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                name={"price"} // ! Updating name field to the right name
+                onChange={handleChange}
+                value={formData.price} // ! Updating the value to the right bit of formData
+              />
+            </div>
           </div>
-        </div>
-        <div className="field">
-          <label className="label">Image URL</label>
-          <div className="control">
-            <input
-              className="input"
-              type="text" // ! No longer a password field
-              name={'image'} // ! Updating name field to the right name
-              onChange={handleChange}
-              value={formData.image} // !  Updating the value to the right bit of formData
-            />
+          <div className="field">
+            <label className="label">Image URL</label>
+            <div className="control">
+              <input
+                className="input"
+                type="text" // ! No longer a password field
+                name={"image"} // ! Updating name field to the right name
+                onChange={handleChange}
+                value={formData.image} // !  Updating the value to the right bit of formData
+              />
+            </div>
           </div>
-        </div>
-        <div className="field">
-          <label className="label">Description</label>
-          <div className="control">
-            <input
-              className="input"
-              type="text" // ! No longer a password field
-              name={'description'} // ! Updating name field to the right name
-              onChange={handleChange}
-              value={formData.description} // !  Updating the value to the right bit of formData
-            />
+          <div className="field">
+            <label className="label">Description</label>
+            <div className="control">
+              <input
+                className="input"
+                type="text" // ! No longer a password field
+                name={"description"} // ! Updating name field to the right name
+                onChange={handleChange}
+                value={formData.description} // !  Updating the value to the right bit of formData
+              />
+            </div>
           </div>
-        </div>
-         <div className="field">
-          <label className="label">Category</label>
-          <div className="control">
-            <input
-              className="input"
-              type="text"
-              name={'category'} // ! Updating name field to the right name
-              onChange={handleChange}
-              value={formData.category} // ! Updating the value to the right bit of formData
-            />
+          <div className="field">
+            <label className="label">Category</label>
+            <div className="control">
+              <select
+                className="input"
+                // type="text"
+                name={"category"} // ! Updating name field to the right name
+                onChange={handleChange}
+                value={formData.category} // ! Updating the value to the right bit of formData
+              >
+                <option value="">Select a cateogry</option>
+                <option value="Pastries">Pastries</option>
+                <option value="Cheese">Cheese</option>
+                <option value="Meat">Meat</option>
+              </select>
+                      </div>
           </div>
-        </div>
-        <button className="button">Submit</button>
-      </form>
+          <button className="button">Submit</button>
+        </form>
+      </div>
     </div>
-  </div>
+  );
 }
--- components/ProductsList.tsx
+++ components/ProductsList.tsx
@@ -5,24 +5,78 @@ import { IProduct } from "../interfaces/productInterface";
 type Products = null | Array<IProduct>;
 
 function ProductsList() {
+  // const [category, setCategory] = React.useState<any>([]);
   const [products, setProducts] = React.useState<Products>(null);
+  const [search, setSearch] = React.useState("");
+  const [value, setValue] = React.useState("");
 
+
+  // FUNCTION TO RETURN ALL DATA
   React.useEffect(() => {
     async function fetchProducts() {
       const resp = await fetch("/api/products");
       const data = await resp.json();
+      console.log("fetch 1", data);
       setProducts(data);
+      console.log("fetch product 1");
     }
     fetchProducts();
   }, []);
+  
+  //  FUNCTION TO RETURN FILTERED DATA
+  React.useEffect(() => {
+    async function fetchProducts() {
+      const resp = await fetch(`/api/${value}`);
+      const productData = await resp.json();
+      setProducts(productData);
+      console.log("fetch product 2");
+    }
+    fetchProducts();
+  }, [value]);
 
-  console.log(products);
+  function handleDropdownChange(e: any) {
+    setValue(e.currentTarget.value);
+  }
+  function handleSearchBarChange(e: any) {
+    setSearch(e.currentTarget.value);
+  }
+
+  const dropdownCategoryOptions = [
+    { label: "Select Category", value: "" },
+    { label: "Pastries", value: "pastries" },
+    { label: "Cheese", value: "Cheese" },
+    { label: "Meat", value: "meat" },
+  ];
+
+  function filterProducts() {
+    return products?.filter((product: any) => {
+      return (
+        (search === "" ||
+          product.title.toLowerCase().includes(search.toLowerCase())) &&
+        (value === "" || product.category.includes(value))
+      );
+    });
+  }
 
   return (
     <section className="section">
       <div className="container">
+        {/* Search bar */}
+        <input
+          className="column is-two-third input mb-4 "
+          placeholder="Search product.."
+          onChange={handleSearchBarChange}
+        />
+        {/* Drop down */}
+        <label className="column drop select is-info mb-4 p-0">
+          <select value={value} onChange={handleDropdownChange}>
+            {dropdownCategoryOptions.map((option: any) => { 
+              return <option value={option.value}>{option.label}</option>;
+            })}
+          </select>
+        </label>
         <div className="columns is-multiline">
-          {products?.map((product) => {
+          {filterProducts()?.map((product: any) => {
             return (
               <ProductCard
                 key={product._id}
